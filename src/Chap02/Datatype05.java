b5892ad08f15f76120120d12eaaae875df6f8e0a
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-25 19:24:52 +0900

    불리언 자료형 변수 선언하고 변숫값 출력

---
+++ src/Chap02/Datatype05.java
@@ -0,0 +1,11 @@
+package Chap02;
+
+public class Datatype05 {
+    public static void main(String[] args) {
+        boolean t = true; // boolean 변수 t 선언 및 true로 초기화
+        boolean f = false; // boolean 변수 f 선언 및 false로 초기화
+
+        System.out.println("t = " + t); // 변수 t 출력
+        System.out.println("f = " + f); // 변수 f 출력
+    }
+}
