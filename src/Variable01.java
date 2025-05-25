1645ac1c43ba151b0bd4ab62f7bf17d49c45c58f
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-25 18:07:43 +0900

    변수 선언, 초기화 및 대입 실습

---
+++ src/Variable01.java
@@ -0,0 +1,13 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Variable01 {
+    public static void main(String[] args) {
+
+        int days = 10;              // 변수 days 선언 및 10으로 초기화
+        System.out.println(days);   // 변수 days 출력
+
+        int speed;                  // 변수 speed 선언
+        speed = 100;                // 변수 speed에 100 대입
+
+    }
+}
