5ec10753deec82216b4d476137fe50932eeca125
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-25 19:16:18 +0900

    정수 자료형 변수 선언 후 변숫값 출력

---
+++ src/Chap02/Datatype01.java
@@ -0,0 +1,14 @@
+package Chap02;
+
+public class Datatype01 {
+    public static void main(String[] args) {
+        int i = 10; // 정수형 변수 a 선언 및 10으로 초기화
+        short s = 20; // 짧은 정수형 변수 b 선언 및 20으로 초기화
+        long l = 30L; // 긴 정수형 변수 c 선언 및 30으로 초기화 (L 접미사 사용 (소문자 1과 l구분))
+        byte b = 50; // 바이트형 변수 b 선언 및 50으로 초기화
+        System.out.println("i = " + i); // 변수 i 출력
+        System.out.println("s = " + s); // 변수 s 출력
+        System.out.println("l = " + l); // 변수 l 출력
+        System.out.println("l = " + b); // 변수 b 출력
+    }
+}
