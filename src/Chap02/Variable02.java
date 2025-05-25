386358562a8223293ac343665219fe7045850d05
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-25 18:33:40 +0900

    상수를 사용하여 원의 넓이 구하기

---
+++ src/Chap02/Variable02.java
@@ -0,0 +1,11 @@
+package Chap02;
+
+public class Variable02 {
+    public static void main(String[] args) {
+
+        final double PI = 3.14; // 상수 PI 선언 및 3.14로 초기화
+
+        System.out.print("반지름 3인 원의 넓이는 : ");
+        System.out.println(PI * 3 * 3); // 원의 넓이 계산 및 출력
+    }
+}
