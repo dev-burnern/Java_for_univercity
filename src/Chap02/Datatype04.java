c789028622589daf01ff0b88d9fdee39c1e27be9
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-25 19:23:40 +0900

    문자열 자료형 변수 선언하고 변숫값 출력

---
+++ src/Chap02/Datatype04.java
@@ -0,0 +1,12 @@
+package Chap02;
+
+public class Datatype04 {
+    public static void main(String[] args) {
+        String str1 = "java"; // 문자열 변수 str1 선언 및 "java"로 초기화
+        String str2 = "programming"; // 문자열 변수 str2 선언 및 "programming"으로 초기화
+
+        System.out.println("str1 = " + str1); // 변수 str1 출력
+        System.out.println("str2 = " + str2); // 변수 str2 출력
+        System.out.println("str1 + str2 = " + str1 + str2); // 변수 str1과 str2의 값을 문자열로 연결하여 출력
+    }
+}
