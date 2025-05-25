1fed052591d760bdbad026c568b61bd722e089e2
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-25 19:26:23 +0900

    문자열과 변수 결합하여 출력

---
+++ src/Chap02/InputOutput01.java
@@ -0,0 +1,12 @@
+package Chap02;
+
+public class InputOutput01 {
+    public static void main(String[] args) {
+
+        String myName = "홍길동"; // 문자열 변수 myName 선언 및 "홍길동"으로 초기화
+        int myAge = 25; // 정수형 변수 myAge 선언 및 25로 초기화
+
+        System.out.println("이름: " + myName); // 변수 myName 출력
+        System.out.println("나이: " + myAge); // 변수 myAge 출력
+    }
+}
