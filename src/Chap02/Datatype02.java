56d78fef1924a6f9a39fa86deedcf586bcc4509e
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-25 19:18:16 +0900

    실수 자료형 변수 선언하고 변숫값 출력

---
+++ src/Chap02/Datatype02.java
@@ -0,0 +1,10 @@
+package Chap02;
+
+public class Datatype02 {
+    public static void main(String[] args) {
+        float f = 214.1421f; // 실수형 변수 f 선언 및 214.1421로 초기화 (f 접미사 사용)
+        double d = 614.1421d; // 배정밀도 실수형 변수 d 선언 및 614.1421로 초기화 (d 접미사 사용, 생략 가능)
+        System.out.println("f = " + f); // 변수 f 출력
+        System.out.println("d = " + d); // 변수 d 출력
+    }
+}
