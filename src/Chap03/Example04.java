f32047f9c95f4b2d90c9e7adcfd131c136e65aeb
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-26 21:00:28 +0900

    할당 연산자 사용 예시

---
+++ src/Chap03/Example04.java
@@ -0,0 +1,14 @@
+package Chap03;
+
+public class Example04 {
+    public static void main(String[] args) {
+        int x = 10; // 변수 x 선언 및 초기화
+        System.out.println(x); // x의 값 출력
+        x += 2; // x에 2를 더함
+        System.out.println(x); // x의 새로운 값 출력
+        x *= 2; // x에 2를 곱함
+        System.out.println(x); // x의 새로운 값 출력
+        x -= 3; // x에서 3을 뺌
+        System.out.println(x); // x의 새로운 값 출력
+    }
+}
