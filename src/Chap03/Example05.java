4735780cb5034ca9749cb8702d44cf9a44fa2ec5
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-26 21:24:54 +0900

    증가 및 감소 연산자 사용 예시

---
+++ src/Chap03/Example05.java
@@ -0,0 +1,12 @@
+package Chap03;
+
+public class Example05 {
+    public static void main(String[] args) {
+        int x = 10; // 변수 x 선언 및 초기화
+        System.out.println(x++); // x의 현재 값 출력 후 1 증가
+        System.out.println(++x); // x를 1 증가시킨 후 현재 값 출력
+        System.out.println(x--); // x의 현재 값 출력 후 1 감소
+        System.out.println(--x); // x를 1 감소시킨 후 현재 값 출력
+
+    }
+}
