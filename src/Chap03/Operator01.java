1f4afdb752f6f133efa7b57a5b6d411216cee3da
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-26 20:11:36 +0900

    산술 연산자를 이용하여 정수와 실수 연산하기

---
+++ src/Chap03/Operator01.java
@@ -0,0 +1,13 @@
+package Chap03;
+
+public class Operator01 {
+    public static void main(String[] args) {
+        int a = 10; // 변수 a 선언 및 10으로 초기화
+        double b = 5.0; // 변수 b 선언 및 5.0으로 초기화
+        System.out.println("a + b = " + (a + b)); // a와 b의 합 출력
+        System.out.println("a - b = " + (a - b)); // a와 b의 차 출력
+        System.out.println("a * b = " + (a * b)); // a와 b의 곱 출력
+        System.out.println("a / b = " + (a / b)); // a와 b의 몫 출력
+        System.out.println("a % b = " + (a % b)); // a와 b의 나머지 출력
+    }
+}
