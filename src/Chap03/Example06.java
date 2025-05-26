fe56529e5625ae73f8ac54573495120221d90ea4
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-26 21:34:27 +0900

    자동 형변환 예시

---
+++ src/Chap03/Example06.java
@@ -0,0 +1,12 @@
+package Chap03;
+
+public class Example06 {
+    public static void main(String[] args) {
+        int i = 100; // 변수 i 선언 및 초기화
+        long l = i; // int형 i를 long형 l로 변환
+        float f = l; // long형 l을 float형 f로 변환
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+    }
+}
