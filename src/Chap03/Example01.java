9454a7dbfb3636d181db488f8269103b4ba834d9
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-26 20:10:08 +0900

    산술 연산자 사용 예시

---
+++ src/Chap03/Example01.java
@@ -0,0 +1,12 @@
+package Chap03;
+
+public class Example01 {
+    public static void main(String[] args) {
+        int x = 10,  y = 20; // 변수 x와 y 선언 및 초기화
+        System.out.println("x + y = " + (x + y)); // x와 y의 합 출력
+        System.out.println("x - y = " + (x - y)); // x와 y의 차 출력
+        System.out.println("x * y = " + (x * y)); // x와 y의 곱 출력
+        System.out.println("x / y = " + (x / y)); // x와 y의 몫 출력
+        System.out.println("x % y = " + (x % y)); // x와 y의 나머지 출력
+    }
+}
