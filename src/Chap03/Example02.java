44d99b8e69dfef553137579989d726bf69210907
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-26 20:13:45 +0900

    관계 연산자 사용 예시

---
+++ src/Chap03/Example02.java
@@ -0,0 +1,9 @@
+package Chap03;
+
+public class Example02 {
+    public static void main(String[] args) {
+        int x = 10, y = 10;
+        System.out.println(x == 10); // x가 10인지 비교하여 true 출력
+        System.out.println("x와 y가 같습니까? " + (x == y)); // x와 y가 같은지 비교하여 true 출력
+    }
+}
