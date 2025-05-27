848398863096b66b9e12cd08319f36738085f4e8
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-27 22:28:52 +0900

    if~else문 사용 예시

---
+++ src/Chap04/Example02.java
@@ -0,0 +1,14 @@
+package Chap04;
+
+public class Example02 {
+    public static void main(String[] args) {
+        int num = 20; // 변수 num 선언 및 초기화
+
+        if (num < 50) {
+            System.out.println("숫자가 50보다 작습니다."); // num이 50보다 작은 경우 출력
+        }
+        else {
+            System.out.println("숫자가 50 이상입니다."); // num이 50 이상인 경우 출력
+        }
+    }
+}
