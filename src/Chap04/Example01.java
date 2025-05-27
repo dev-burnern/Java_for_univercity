bbc95fc1e37bb77587ee3b99b96f51f3525af64c
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-27 21:59:42 +0900

    If문 사용 예시

---
+++ src/Chap04/Example01.java
@@ -0,0 +1,12 @@
+package Chap04;
+
+public class Example01 {
+    public static void main(String[] args) {
+        int num = 10; // 변수 num 선언 및 초기화
+
+        if (num > 15){
+            System.out.println("숫자가 15보다 큼");
+        }
+        System.out.println("If 문을 빠져나옴");
+    }
+}
