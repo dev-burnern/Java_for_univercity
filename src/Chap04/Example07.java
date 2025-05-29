17297b49bb2133e7c5f67fefb49cb915c7acc29d
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-29 21:05:00 +0900

    for문 사용 예시

---
+++ src/Chap04/Example07.java
@@ -0,0 +1,10 @@
+package Chap04;
+
+public class Example07 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i = 1; i <= 10; i++) { // i가 1부터 10까지 반복
+            System.out.println("i = " + i); // 현재 i의 값을 출력
+    }
+}
