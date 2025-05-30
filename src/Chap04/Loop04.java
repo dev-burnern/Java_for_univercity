f806c1737e530bae7edfd0507872a5fc849ca8df
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-30 14:41:09 +0900

    별표로 역삼각형 만들기

---
+++ src/Chap04/Loop04.java
@@ -0,0 +1,22 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class Loop04 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in); // Scanner 객체 생성
+        System.out.print("길이 : ");
+
+        int len = s.nextInt();
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < (2 * len)-1-(i*2); j++) {
+                System.out.print("*");
+            }
+            System.out.println(""); // 줄바꿈
+        }
+    }
+}
