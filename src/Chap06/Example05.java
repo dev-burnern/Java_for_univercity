262094c8074d29e1267af6f0011d3c9546a7e8c7
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-30 22:13:33 +0900

    문자열 선언 예시

---
+++ src/Chap06/Example05.java
@@ -0,0 +1,15 @@
+package Chap06;
+
+public class Example05 {
+    public static void main(String[] args) {
+        String s1 = "Java"; // 문자열 s1 선언 및 초기화
+        String s2 = "Java"; // 문자열 s2 선언 및 초기화
+        String s3 = new String("Java"); // 문자열 s3를 new 키워드로 생성
+        String s4 = new String("Java"); // 문자열 s4를 new 키워드로 생성
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
