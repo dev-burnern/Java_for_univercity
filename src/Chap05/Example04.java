2cbaf850a29bf5c64da439f1daeb544845a331c9
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-30 15:17:30 +0900

    입력과 출력이 없는 메서드 예시

---
+++ src/Chap05/Example04.java
@@ -0,0 +1,11 @@
+package Chap05;
+
+public class Example04 {
+    public static void print(){
+        System.out.println("Hello, World!"); // "Hello, World!" 출력
+    }
+
+    public static void main(String[] args) {
+        print();
+    }
+}
