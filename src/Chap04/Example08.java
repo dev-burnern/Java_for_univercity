dc7c3cff1b475ab81e015995a35791893e0b646c
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-30 14:16:53 +0900

    while문 사용 예시

---
+++ src/Chap04/Example08.java
@@ -0,0 +1,11 @@
+package Chap04;
+
+public class Example08 {
+    public static void main(String[] args) {
+        int i = 1; // 변수 i를 1로 초기화
+        while (i <= 10) { // i가 10 이하인 동안 반복 : 총 10번 반복
+            System.out.print(i + " "); // 현재 i의 값을 출력
+            i++; // i를 1 증가시킴
+        }
+    }
+}
