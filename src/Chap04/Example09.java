5edf326d32650fc29981e698689508df62efa2c5
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-30 14:25:28 +0900

    do~while문 사용 예시

---
+++ src/Chap04/Example09.java
@@ -0,0 +1,12 @@
+package Chap04;
+
+public class Example09 {
+    public static void main(String[] args) {
+        int i = 1; // 변수 i를 1로 초기화
+        do {
+            System.out.println("Hello, World!"); // "Hello, World!" 출력
+            i++; // i를 1 증가시킴
+        }
+        while (i < 6); // i가 6보다 작을 때 까지 반복 총 1부터 5까지 5번 출력
+    }
+}
