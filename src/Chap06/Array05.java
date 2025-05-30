51e244d273d2f453065466ea2a004aa1ba6e52c4
dev-burnern <200775118+dev-burnern@users.noreply.github.com>
2025-05-30 23:16:12 +0900

    String을 이용하여 문자열 저장하고 출력하기

---
+++ src/Chap06/Array05.java
@@ -0,0 +1,11 @@
+package Chap06;
+
+public class Array05 {
+    public static void main(String[] args) {
+        String s1 = "Java Programming"; // 문자열 s1 선언 및 초기화
+        String s2 = new String("Java Programming"); // 문자열 s2를 new 키워드로 생성
+
+        System.out.println(s1); // s1 출력
+        System.out.println(s2); // s2 출력
+    }
+}
